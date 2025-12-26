74dec960f89d34c3b45057e965c1ffa8efcbaf04
Manish Kumar Sharma <manish.sharma@falconsystem.com>
2025-12-26 18:23:47 +0530

    index.js added in this project

---
+++ index.js
@@ -0,0 +1,11 @@
+const name = "dronna";
+
+if (name == "demo") {
+     console.log("success your data have been configurations");
+
+     for (let index = 0; index < array.length; index++) {
+          const element = array[index];
+     }
+} else {
+     console.log("data not fetched from sever and please configurations and try again");
+}
